499d2309168632795d8ea6cfb5141c1ea344dc54
Ahmad Fauzan <fauzan18296@gmail.com>
2024-07-10 21:15:33 +0700

    add:Melanjutkan materi bagian routes dan controller

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,8 +1,16 @@
  const getAllUser = (req,res) =>{
+  const data = {
+    id: '1',
+    name:'Ahmad Fauzan',
+    email: 'fauzan18296@gmail.com',
+    addres: 'Surabaya'
+  }
   res.json({
     message: 'GET all users is success!',
+    data: data
   })
 } 
+
  const createNewUser = (req,res) => {
   console.log(req.body)
   res.json({
@@ -10,4 +18,26 @@
     data: req.body
   })
 }
-export {getAllUser, createNewUser}
+
+const updateUser = (req,res) => {
+  const {id} = req.params;
+  console.log('id:', id);
+  res.json({
+    message: 'UPDATE user is success!',
+    data: req.body
+  })
+}
+
+const deleteUser = (req, res) => {
+  const {id} = req.params;
+  res.json({
+    message: 'DELETE user is success!',
+    data: {
+      id: id,
+      name: 'Ahmad Fauzan',
+      email: 'fauzan18296@gmail.com',
+      address: 'Surabaya',
+    }
+  })
+}
+export {getAllUser, createNewUser, updateUser, deleteUser}
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import {getAllUser, createNewUser} from '../controller/users.js'
+import {getAllUser, createNewUser, updateUser, deleteUser} from '../controller/users.js'
 const route = express.Router();
 
 // Create  - Method POST
 route.post('/',createNewUser)
 // Read - Method Get
 route.get('/', getAllUser)
-
+// Update data tanpa mengubah semua datanya - Method Patch
+route.patch('/:id', updateUser)
+// Delete - Method Delete
+route.delete('/:id', deleteUser)
 export default route
