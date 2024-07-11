faca4b342dab39fffe820297d4c97231418c3d52
Ahmad Fauzan <fauzan18296@gmail.com>
2024-07-12 00:00:31 +0700

    add: Melanjutkan materi tetapi masih belum tuntas

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import express from 'express';
 import userRoutes from './routes/users.js'
 import middlewareLogRequest from './middleware/logs.js'
+import mysql from 'mysql2'
 const app = express();
+
+const db = mysql.createPool({
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'express_mysql',
+});
 
 app.use(middlewareLogRequest)
 app.use(express.json())
 
 app.use('/users', userRoutes)
+app.use('/', (req,res) => {
+  const sql = 'SELECT * FROM users'
+  db.execute(sql, (rows) => {
+    try{
+      res.status(200).json({
+        message: `connection is success!`,
+        data: rows,
+      })
+    }catch(error) {
+      console.error(`Error fetching users: ${error}`)
+      res.status(500).json({
+        message:`connection is failed because ${error.message}`,
+        error:error
+      })
+    }
+  }) 
+})
 
 const port = 5000;
 app.listen(port, () => {
