4bbe87e47cc27c1fba88cb9b832fa669169fd657
Ahmad Fauzan <fauzan18296@gmail.com>
2024-07-14 21:12:45 +0700

    fix: connection mysql-server

--- src/index.js
+++ src/index.js
@@ -1,39 +1,41 @@
 import express from 'express';
-import userRoutes from './routes/users.js'
-import middlewareLogRequest from './middleware/logs.js'
-import mysql from 'mysql2'
+import userRoutes from './routes/users.js';
+import middlewareLogRequest from './middleware/logs.js';
+import mysql from 'mysql2';
+
 const app = express();
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'express_mysql',
+const pool = mysql.createPool({
+  host: "localhost",
+  user: "root",
+  password: "P@ssword123",
+  database: "express_mysql_rest_api",
 });
 
-app.use(middlewareLogRequest)
-app.use(express.json())
+app.use(middlewareLogRequest);
+app.use(express.json());
 
-app.use('/users', userRoutes)
-app.use('/', (req,res) => {
-  const sql = 'SELECT * FROM users'
-  db.execute(sql, (rows) => {
-    try{
-      res.status(200).json({
-        message: `connection is success!`,
-        data: rows,
-      })
-    }catch(error) {
-      console.error(`Error fetching users: ${error}`)
+app.use('/users', userRoutes);
+
+app.use('/', (req, res) => {
+  pool.execute('SELECT * FROM users', (error, rows) => {
+    if (error) {
+      console.error(`Error fetching users: 
+        ${error}`);
       res.status(500).json({
-        message:`connection is failed because ${error.message}`,
-        error:error
-      })
-    }
-  }) 
-})
+        message: `Connection failed because ${error.message}`,
+        error: error
+      });
+    }else {
+    res.status(200).json({
+      message: 'Connection is successful!',
+      data: rows
+    });
+  }
+  });
+});
 
 const port = 5000;
-app.listen(port, () => {
-  console.log(`Server success running in ${port}`);
-})
+app.listen(port,() => {
+  console.log(`Server is running successfully on port ${port}`);
+});
